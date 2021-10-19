13dc19cd20aa7e8107075503f3bb7c672bf97a04
ChristopherChiasson139 <Christopher139139@gmail.com>
2021-10-19 09:46:59 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace C_FundamentalsLoopsGame_Loop
     {
         static void Main(string[] args)
         {
+            int y;
+            int x;
+            
+            //Console.SetCursorPosition(y,x);
+
+
+            Console.WriteLine("game loop");
+            bool gameOver = false;
+
+
+            while (gameOver == false)
+            {
+                char input;
+                ConsoleKeyInfo readKeyInput = Console.ReadKey(true);
+                input = readKeyInput.KeyChar;
+
+                Console.WriteLine("user input = " + input);
+            }
+            Console.WriteLine("game over!");
+            Console.ReadKey(true);
         }
     }
 }
